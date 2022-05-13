99b8d0dde89d6e31a67311a277e05c66ee9e8dd7
shafiul-naim <shafiul.naim@gmail.com>
2022-05-13 22:40:49 +0600

    load data

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
 
 app.use(cors());
 app.use(express.json());
 
 
-// inventoryUser
-// SEZ1RXUIQ9lID7Fj
-
-
-
-const uri = "mongodb+srv://inventoryUser:SEZ1RXUIQ9lID7Fj@cluster0.gmdaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("DB connected")
-  // perform actions on the collection object
-  client.close();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmdaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log("db connected")
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const inventoriesCollection = client
+      .db("bookKeeper")
+      .collection("inventory");
 
+    //load inventories
 
+    app.get("/inventories", async (req, res) => {
+        const query = {};
+        const cursor = inventoriesCollection.find(query);
+        const inventories = await cursor.toArray();
+        res.send(inventories);
+      });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Assignment-11 server is running");
 });
 
-
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
-    console.log("Listening to port", port);
-  });
+  console.log("Listening to port", port);
+});
+
+
+
+
